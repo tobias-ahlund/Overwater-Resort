53334357e57b0dd058278401ce289056bbc8cf83
MagnusVV <svartkub@msn.com>
2023-01-26 22:51:07 +0100

    Booking fixes
    I realised afterwards that there could be issues if NO extra features were selected. That is corrected now.

--- receipt.php
+++ receipt.php
@@ -7,6 +7,25 @@ declare(strict_types=1);
 //Magnus V. - Added the array with selected extra features, to be added to the receipt.
 function receipt($arrDate, $depDate, $totalCost, $selectedExtraFeatures)
 {
+
+    $cleanFeaturesList = [];
+
+    foreach ($selectedExtraFeatures as $selectedExtraFeature) {
+        if ($selectedExtraFeature['feature'] != "") {
+            foreach ($selectedExtraFeatures as $selectedExtraFeature) {
+                if ($selectedExtraFeature['feature'] != "") {
+                    $cleanFeaturesList[] = $selectedExtraFeature;
+                }
+            }
+            break;
+        }
+    }
+
+    if ($cleanFeaturesList == null || empty($cleanFeaturesList)) :
+        $featuresReceipt = "No extras selected.";
+    else : $featuresReceipt = $cleanFeaturesList;
+    endif;
+
     $receiptContent = [
         "island" => "Isla de los Monos",
         "hotel" => "Overwater Resort",
@@ -14,7 +33,7 @@ function receipt($arrDate, $depDate, $totalCost, $selectedExtraFeatures)
         "departure_date" => $depDate,
         "total_cost" => $totalCost,
         "stars" => "3",
-        "features" => $selectedExtraFeatures,
+        "features" => $featuresReceipt,
         "additional_info" => "Thank you for choosing Overwater Resort."
     ];
 
